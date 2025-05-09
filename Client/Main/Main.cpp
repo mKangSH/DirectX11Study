4b6e5f3ccea0ebb8970bde4bf80939e2e85785dd
shkang2 <shkang2@piegroup.co.kr>
2025-05-09 10:50:11 +0900

    Initial Commit

--- Client/Main/Main.cpp
+++ Client/Main/Main.cpp
@@ -3,7 +3,7 @@
 #include "Engine/02.Managers/Game.h"
 #include "../Game/01. TriangleDemo.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	GameDesc desc;
 	desc.appName = L"GameCoding";
